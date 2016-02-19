8f9ccad2505c85ad8d29595ff1a8bba6a5193fc9
peterysc <peterysc@gmail.com>
2016-02-18 17:37:13 -0800

    queueCircularArray size done

--- Queue/src/Queue/CircularArrayQueue.java
+++ Queue/src/Queue/CircularArrayQueue.java
@@ -1,5 +1,71 @@
 package Queue;
 
 public class CircularArrayQueue {
+	
+	private int front;
+	private int back;
+	private int capacity; // size = total number of elements in the array
+	private int[] array;
+	
+	private CircularArrayQueue(int size){
+		capacity = size;
+		front = -1;
+		back = -1;
+		array = new int[size];
+	}
 
+	public static CircularArrayQueue createQueue(int size){
+		
+		CircularArrayQueue queue = new CircularArrayQueue(size);
+		queue.front = 0;
+		queue.back = 0;
+		
+		return queue;
+		
+	}
+	
+	public boolean isEmpty(){
+		
+		return (front == -1);
+		
+	}
+	
+	public boolean isFull(){
+		return ((back+1)%capacity == front);
+	}
+	
+	public int getQueueSize(){
+		// if the queue is full, the size = capacity
+		if(isFull()){
+			return capacity;
+		}
+		// if the queue is empty, the size = 0
+		if(isEmpty()){
+			return 0;
+		}
+		if(front > back){
+			return ((capacity - (front - back) +1) %capacity);
+		}
+		return((capacity + (front - back))%capacity);	
+	}
+	
+	public void enQueue(int data){
+		if(back == 0){
+			array[back] = data;
+			back ++;
+		}
+		
+		array[back] = data;
+		back++;
+		
+	}
+	
+	public int deQueue(){
+		return 0;
+	}
+	
+	public void deleteQueue(){
+		front = -1;
+		back = -1;
+	}
 }
