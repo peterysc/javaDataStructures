8726cc2a1ff935cbe0c9dc1547460b36416ff73b
peterysc <peterysc@gmail.com>
2016-02-18 20:31:53 -0800

    binary tree in java

---
+++ BinaryTreeNode/src/BinaryTreeNode/BinaryTreeNode.java
@@ -0,0 +1,33 @@
+package BinaryTreeNode;
+
+public class BinaryTreeNode {
+	
+	private char data;
+	private BinaryTreeNode left;
+	private BinaryTreeNode right;
+	
+	public char getData(){
+		return data;
+	}
+	
+	public void setData(char data){
+		this.data = data;
+	}
+	
+	public BinaryTreeNode getLeft(){
+		return left;
+	}
+	
+	public void setLeft(BinaryTreeNode left){
+		this.left = left;
+	}
+	
+	public BinaryTreeNode getRight(){
+		return right;
+	}
+	
+	public void setRight(BinaryTreeNode right){
+		this.right = right;
+	}
+	
+}
