d51ed166a6b87e2c75a3c5fff7e337add324b79a
peterysc <peterysc@gmail.com>
2016-02-18 18:38:05 -0800

    dynamic array queue basic began

--- Queue/src/Queue/DynamicArrayQueue.java
+++ Queue/src/Queue/DynamicArrayQueue.java
@@ -1,5 +1,38 @@
 package Queue;
 
 public class DynamicArrayQueue {
+	
+	private int capacity;
+	private int front;
+	private int back;
+	private char[] array;
+	
+	public static DynamicArrayQueue createDynamicArrayQueue(){
+		return new DynamicArrayQueue();
+	}
+	
+	public booelan isEmpty(){
+		
+	}
+	
+	public boolean isFull(){
+		
+	}
+	
+	public int getQueueSize(){
+		
+	}
+	
+	private void resizeQueue(){
+		
+	}
+	
+	public void enQueue(){
+		
+	}
+	
+	public int deQueue(){
+		
+	}
 
 }
