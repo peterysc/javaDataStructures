04c009c9f8905b89d5e625c3b75102db89bbe6f9
peterysc <peterysc@gmail.com>
2016-02-19 21:18:10 -0800

    bst find complete

---
+++ BinarySearchTree/src/BSTNode/BSTNode.java
@@ -0,0 +1,64 @@
+package BSTNode;
+
+public class BSTNode {
+	private int data;
+	private BSTNode left;
+	private BSTNode right;
+	
+	public int getData(){
+		return data;
+	}
+	
+	public void setData(int data){
+		this.data = data;
+	}
+	
+	public BSTNode getLeft(){
+		return left;
+	}
+	
+	public BSTNode getRight(){
+		return right;
+	}
+	
+	public void setLeft(BSTNode left){
+		this.left = left;
+	}
+	
+	public void setRight(BSTNode right){
+		this.right = right;
+	}
+	
+	public BSTNode find(BSTNode root, int data){
+		if(root == null){
+			return null;
+		}
+		
+		BSTNode currentNode = root;
+		int currentNodeData = currentNode.getData();
+		
+		// root data is equal to the data looking for
+		if(currentNodeData == data){
+			return root;
+		}
+		while(currentNode != null){
+			// traverse left subtree if data < currentNodeData
+			if(currentNodeData > data){
+				currentNode = currentNode.getLeft();
+				currentNodeData = currentNode.getData();
+			}
+			// traverse right subtree if data > currentNodeData
+			if(currentNodeData < data){
+				currentNode = currentNode.getRight();
+				currentNodeData = currentNode.getData();
+			}
+			if(currentNodeData == data){
+				return currentNode;
+			}
+		}
+		
+		// nothing found
+		
+		return null;
+	}
+}
